fe08f0af684d11996fc08e509ee5eac0c291ed1f
scottncoleman <sc15358@my.bristol.ac.uk>
2018-02-06 11:11:31 +0000

    Stem removed

--- zoe/EM.py
+++ zoe/EM.py
@@ -7,6 +7,12 @@ import cv2
 from sklearn.mixture import GaussianMixture
 from scipy.stats import multivariate_normal
 from sklearn.cluster import KMeans
+from skimage.morphology import erosion, dilation, opening, closing, white_tophat
+from skimage.morphology import black_tophat, skeletonize, convex_hull_image
+from skimage.morphology import disk
+from scipy.signal import convolve2d
+
+
 
 def CutOut(y1):
     y = cv2.blur(y1,(1,1))
@@ -25,21 +31,21 @@ def CutOut(y1):
             SV[i,0] = Saturation[i]
             SV[i,1] = Value[i]
     
-    max1 = np.max(SV[:,0])
-    max2 = np.max(SV[:,1])
+    max1 = 1#np.max(SV[:,0])
+    max2 = 1#np.max(SV[:,1])
     #max3 = np.max(lu[:,2])
     SV[:,0] = SV[:,0]/max1
     SV[:,1] = SV[:,1]/max2
     #lu[:,2] = lu[:,2]/max3
     gmm.fit(SV)
     
-#    X, Y = np.meshgrid(np.linspace(0, 1,256), np.linspace(0,1,200))
-#    XX = np.array([X.ravel(), Y.ravel()]).T
-#    Z = gmm.score_samples(XX)
-#    Z = Z.reshape((200,256))
-#    plt.scatter(SV[:, 0], SV[:, 1],0.25,'b')
-#    plt.contour(X, Y, Z)
-#    plt.show()
+    X, Y = np.meshgrid(np.linspace(0, 1,256), np.linspace(0,1,200))
+    XX = np.array([X.ravel(), Y.ravel()]).T
+    Z = gmm.score_samples(XX)
+    Z = Z.reshape((200,256))
+    plt.scatter(SV[:, 0], SV[:, 1],0.25,'b')
+    plt.contour(X, Y, Z)
+    plt.show()
     
     x = np.random.randint(2, size=(shape(y)[0], shape(y)[1]))
     
@@ -71,40 +77,50 @@ def CutOut(y1):
                 x[j,i] = 1
             else:
                 x[j,i] = 0
-                
+    x = erosion(x, disk(5))
+    x = dilation(x, disk(4))
+    plt.imshow(x)
+    plt.show()
+
+    s = skeletonize(x == 1)
+
     d = y1
     d[:,:,0] = x*y1[:,:,0]
     d[:,:,1] = x*y1[:,:,1]
     d[:,:,2] = x*y1[:,:,2]
-    return x,d
+
+    return x,d,s
     
 
-#y1 = imread('wb1127-05-2.jpg')
-#x,d = CutOut(y1)
-#
-#plt.imshow(x)
-#plt.show()
-#plt.imshow(d)
-#plt.show()
-#
+y1 = imread('ny1053-04-2.jpg')
+plt.imshow(y1)
+plt.show()
+x,d,s = CutOut(y1)
+
+plt.imshow(s)
+plt.show()
+
+plt.imshow(d)
+plt.show()
+
 #y = imread('wb1127-03-2.jpg')
-input_path = './Leaf_Samples/'
-output_path = './output_segments/'
-
-folders = next(os.walk(input_path))[1]
-
-for folder in folders:
-    files = next(os.walk(input_path+folder))[2]
-	
-	#create the output folder if it doesn't exist
-    if not os.path.exists(output_path + folder):
-        os.makedirs(output_path + folder)
-	
-	#go through all the images under this label (folder)
-    for file in files:
-        print('yay')
-        y = imread(input_path+folder+'/'+file)
-        y = y[1:550,1:550,:]
-        x,d = CutOut(y)
-        imsave(output_path + folder + '/' + file, x)
+#input_path = './Leaf_Samples/'
+#output_path = './output_segments/'
+#
+#folders = next(os.walk(input_path))[1]
+#
+#for folder in folders:
+#    files = next(os.walk(input_path+folder))[2]
+#	
+#	#create the output folder if it doesn't exist
+#    if not os.path.exists(output_path + folder):
+#        os.makedirs(output_path + folder)
+#	
+#	#go through all the images under this label (folder)
+#    for file in files:
+#        print('yay')
+#        y = imread(input_path+folder+'/'+file)
+#        y = y[1:550,1:550,:]
+#        x,d = CutOut(y)
+#        imsave(output_path + folder + '/' + file, x)
 		
