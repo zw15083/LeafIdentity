81a9273433880f91e5e2619e0a49a2a3df8b0742
unknown <tw15220@my.bristol.ac.uk>
2018-02-10 18:01:39 +0000

    almost got radius vs angle

--- feature3.py
+++ feature3.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy.linalg import norm
 import cv2
 import matplotlib.pyplot as plt
 from scipy.misc import imread
@@ -6,48 +7,70 @@ from scipy.misc import imshow
 np.set_printoptions(threshold=np.inf)
 
 def main():
-    img=cv2.imread('leaf1.jpg')
+    img=cv2.imread('weedcolour.jpg')
     
     grey = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     _, thresh = cv2.threshold(grey, 127, 255,
                                cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
     edges=cv2.Canny(thresh,100,200)
+    #grey[:,538]=255
+    #grey[298,:]=255
     
+    #cv2.imshow('edges', grey)
     nnz=np.nonzero(edges)
     
-    #plt.plot(nnz[1],nnz[0],'.')#visualise the boundaries
+    #print(len(nnz[1]))
     
+    #plt.plot(nnz[1],nnz[0],'.')#visualise the boundaries
+    #'''
     #####FIND CENTROID
-    sourceIm,contours, hierarchy = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    '''
+    important: centroid is done on original image (before edge detection is made), otherwise centroid is shifted.
+    '''
+    sourceIm,contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     #print(np.shape(contour[0]))
     cnt = max(contours, key = lambda x: cv2.contourArea(x))
     M = cv2.moments(cnt)
-    cCol = int(M['m10']/M['m00'])
-    cRow = int(M['m01']/M['m00'])       
+    cx = int(M['m10']/M['m00'])
+    cy = int(M['m01']/M['m00'])       
     ###### 
+    #edges[cy,:]=255
+    #edges[:,cx]=255
+    #cv2.imshow('edges', edges)
+    ##arbitrary point
+    ax=cx-20
+    ay=cy
     
-    deltaY = nnz[0] - cRow
-    deltaX = nnz[1] - cCol
-    #print(cx,cy)
-    #print(deltaX)
-    #print(np.where(nnz[1] == cCol))
-    #print(cCol)
-    angles=np.zeros(len(deltaY))
-    for i in range(0,len(deltaY)):
-        if deltaX[i]!=0:
-            angles[i] = np.arctan(deltaY[i]/deltaX[i])
-        elif nnz[0][i]>cRow:
-            angles[i]=0
-        else:
-            angles[i]=0
+    ##normalised vector
+    ux=ax-cx
+    uy=ay-cy
+    u=[ux,uy]
+    #edge vector
+    vx = nnz[1] - cx
+    vy = nnz[0] - cy
+    print(cx,cy)
+    
+    
+    angles=np.zeros(len(nnz[0]))
+    radius=np.zeros(len(nnz[0]))
+    for i in range(0,len(nnz[0])):
+        angles[i]=np.arccos(np.dot(u,[vx[i],vy[i]])/(norm(u)*norm([vx[i],vy[i]])))
+        radius[i]=norm([vx[i],vy[i]])
+        #if norm([vx[i],vy[i]])==0:
+            #print(vx[i],vy[i])
+            #print(nnz[0][i],nnz[1][i])
+    #print(angles[100]*(180/np.pi))
+    #'''     
         
-    #print(sorted(angles*100))       
-    distances = np.sqrt((nnz[0] - cRow)**2 + (nnz[1] - cCol)**2)
-    plt.plot(angles*100,distances,'.')  
+   
+      
+    #distances = np.sqrt((nnz[0] - cRow)**2 + (nnz[1] - cCol)**2)
+    plt.plot(angles*(180/np.pi),radius,'.')  
+    #'''
     '''visualise centroid set to 255 for white lines
     edges[127,:]=0 
     edges[:,128]=0  
-    '''    
+    #'''    
     #cv2.imshow('edges', edges)
     
     
