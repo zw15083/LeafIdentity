eda4169fca0f5cf30d219acd34dc259ff782b11f
unknown <tw15220@my.bristol.ac.uk>
2018-02-19 15:36:18 +0000

    Scotts stuff

---
+++ EM.py
@@ -0,0 +1,111 @@
+from numpy import *
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.misc import *
+import os
+import cv2
+from sklearn.mixture import GaussianMixture
+from scipy.stats import multivariate_normal
+from skimage.morphology import erosion, dilation
+from skimage.morphology import disk
+from scipy import ndimage
+
+def CutOut(y):
+    hsv = cv2.cvtColor(y, cv2.COLOR_BGR2HSV)
+    hsv_shape = hsv.shape
+    gmm = GaussianMixture(n_components=2,covariance_type='diag')
+    SV = np.zeros((hsv_shape[0]*hsv_shape[1],2))
+
+    Saturation = hsv[:,:,1].flatten()
+    Value = hsv[:,:,2].flatten()
+    P = []
+
+    SV[:,0] = Saturation
+    SV[:,1] = Value
+    
+    max1 = np.max(Saturation[:])
+    max2 = np.max(Value[:])
+
+    SV[:,0] = SV[:,0]/max1
+    SV[:,1] = SV[:,1]/max2
+
+    gmm.fit(SV)
+    
+    
+    for i in range(shape(y)[0]):
+        p = []
+        for j in range(shape(y)[1]):
+            p.append([hsv[i,j,1]/max1,hsv[i,j,2]/max2])
+        P.append(p)
+
+
+    if gmm.means_[1][1] > gmm.means_[0][1]:
+        mu1 = gmm.means_[0]
+        mu2 = gmm.means_[1]
+        gmm.covariances_[1] = 100*gmm.covariances_[1]
+        cov1 = gmm.covariances_[0]
+        cov2 = gmm.covariances_[1]
+    else:
+        mu2 = gmm.means_[0]
+        mu1 = gmm.means_[1] 
+        gmm.covariances_[0] = 100*gmm.covariances_[0]
+        cov2 = gmm.covariances_[0]
+        cov1 = gmm.covariances_[1]    
+        
+#    X, Y = np.meshgrid(np.linspace(0, 1,256), np.linspace(0,1,200))
+#    XX = np.array([X.ravel(), Y.ravel()]).T
+#    Z = gmm.score_samples(XX)
+#    Z = Z.reshape((200,256))
+#    plt.scatter(SV[:, 0], SV[:, 1],0.25,'b')
+#    plt.xlabel('Saturation')
+#    plt.ylabel('Value')
+#    plt.contour(X, Y, Z)
+#    plt.show()
+#    
+#    print(gmm.score_samples)
+    
+    p1 = multivariate_normal(mu1, cov1)
+    p2 = multivariate_normal(mu2, cov2)
+    
+    p1 = p1.pdf(P)
+    p2 = p2.pdf(P)
+    
+    x1 = np.heaviside(p1-p2,0)
+    
+    x2 = ndimage.binary_fill_holes(x1).astype(int)
+    
+    s1 = erosion(x1, disk(3))
+    s1 = dilation(s1, disk(3))
+    
+    s2 = erosion(x2, disk(3))
+    s2 = dilation(s2, disk(3))
+
+    return x1,s1,x2,s2
+    
+#y = imread('wb1127-03-2.jpg')
+input_path = './Leaf_Samples/'
+#output_pathx1 = './output_segmentsx1/'
+#output_paths1 = './output_segmentss1/' 
+#output_pathx2 = './output_segmentsx2/'
+output_paths2 = './output_segmentss2/' 
+#
+folders = next(os.walk(input_path))[1]
+#
+for folder in folders:
+    files = next(os.walk(input_path+folder))[2]
+	
+	#create the output folder if it doesn't exist
+    if not os.path.exists(output_paths2 + folder):
+        os.makedirs(output_paths2 + folder)
+	
+	#go through all the images under this label (folder)
+    for file in files:
+        print('yay')
+        y = imread(input_path+folder+'/'+file)
+        x1,s1,x2,s2 = CutOut(y)
+#        imsave(output_pathx1 + folder + '/' + file, x1)
+#        imsave(output_paths1 + folder + '/' + file, s1)
+#        imsave(output_pathx2 + folder + '/' + file, x2)
+        imsave(output_paths2 + folder + '/' + file, s2)
+
+		
--- feature3.1.py
+++ feature3.1.py
@@ -16,22 +16,170 @@ def ExtractReg(angles,radius,n):
     error = abs(radius - fitfn(angles))
     return ang,rad,fit,fitfn,error
 
+def Loco(nnz):
+    N = 10
+    
+    x = nnz[1]
+    y = nnz[0]
+    
+    K = x.size
+    
+    print(x[-2])
+    print(x[-1])
+    print(y[-2])
+    print(y[-1])
+    
+    deltax = np.zeros(K)
+    deltay = np.zeros(K)
+    
+    for i in range(1,K):
+        deltax[i] = x[i] - x[i-1]
+        deltay[i] = y[i] - y[i-1]
+#    deltax = x[1:-1] - x[0:-2]
+#    deltay = y[1:-1] - y[0:-2]
+    
+    print(deltax[-1]==x[-1]-x[-2])
+    
+    deltat = (deltax**2 + deltay**2)**0.5
+    
+    t = np.zeros(K)
+    
+    for i in range(1,K):
+        t[i] = t[i-1] + deltat[i]
+    
+    T=t[-1]
+    
+    xi = np.zeros(K)
+    epsilon = xi
+    
+    sumdeltaxj = xi
+    sumdeltayj = xi
+    
+    print(K)
+    
+    for i in range(2,K):
+        sumdeltaxj[i] = sumdeltaxj[i-1] + deltax[i-1] 
+        sumdeltayj[i] = sumdeltayj[i-1] + deltay[i-1] 
+#        if deltat[i] == 0:
+#            print(i)
+        xi[i] = sumdeltaxj[i]-deltax[i]/deltat[i]*t[i-1]
+        epsilon[i] = sumdeltayj[i]-deltay[i]/deltat[i]*t[i-1]
+        
+    alpha = np.zeros(N)
+    beta = alpha
+    gamma = alpha
+    delta = alpha
+    
+    for i in range(1,K):
+        alpha[0]=alpha[0]+(deltax[i]/(2*deltat[i])*(t[i]**2-t[i-1]**2)+xi[i]*(t[i]-t[i-1]))/T
+        gamma[0]=gamma[0]+(deltay[i]/(2*deltat[i])*(t[i]**2-t[i-1]**2)+epsilon[i]*(t[i]-t[i-1]))/T
+        
+        for j in range(1,N):
+            alpha[j]=alpha[j] + deltax[i]/deltat[i]*(np.cos(2*j*np.pi*t[i]/T)-np.cos(2*j*np.pi*t[i-1]/T))
+            beta[j]=beta[j] + deltax[i]/deltat[i]*(np.sin(2*j*np.pi*t[i]/T)-np.sin(2*j*np.pi*t[i-1]/T))
+            gamma[j]=gamma[j] + deltay[i]/deltat[i]*(np.cos(2*j*np.pi*t[i]/T)-np.cos(2*j*np.pi*t[i-1]/T))
+            delta[j]=delta[j] + deltay[i]/deltat[i]*(np.sin(2*j*np.pi*t[i]/T)-np.sin(2*j*np.pi*t[i-1]/T))
+            
+    for i in range(1,N):
+        alpha[j] = alpha[j] * T/(2*(j*np.pi)**2)
+        beta[j] = beta[j] * T/(2*(j*np.pi)**2)
+        gamma[j] = gamma[j] * T/(2*(j*np.pi)**2)
+        delta[j] = delta[j] * T/(2*(j*np.pi)**2)
 
+    tau = 0.5* np.arctan2(2*(alpha[1]*beta[1]+gamma[1]*delta[1]),alpha[1]**2+gamma[1]**2-beta[1]**2-delta[1]**2)
+    
+    alphaprime=alpha[1]*np.cos(tau)+beta[1]*np.sin(tau)
+    gammaprime=gamma[1]*np.cos(tau)+delta[1]*np.sin(tau)
+    
+    rho=np.arctan2(gammaprime,alphaprime)
+    
+    if rho<0:
+        tau = tau+np.pi
+        
+    alphastar = np.zeros(N)
+    betastar = alphastar
+    gammastar = alphastar
+    deltastar = alphastar
+    
+    for i in range(1,N):
+        alphastar[i] = alpha[i]*np.cos(i*tau) + beta[i]*np.sin(i*tau)
+        betastar[i] = -alpha[i]*np.sin(i*tau) + beta[i]*np.cos(i*tau)
+        gammastar[i] = gamma[i]*np.cos(i*tau) + delta[i]*np.sin(i*tau)
+        deltastar[i] = -gamma[i]*np.sin(i*tau) + delta[i]*np.cos(i*tau)
+    
+    r = alpha[1]*delta[1]-beta[1]*gamma[1]
+    
+    if r<0:
+        betastar = -betastar
+        deltastar = -deltastar
+        
+    a = alphastar
+    b = betastar
+    c = gammastar
+    d = deltastar
+    
+    phi = 0.5*np.arctan2(2*(a*b+c*d),a**2+c**2-b**2-d**2)
+    
+    aprime=a*np.cos(phi)+b*np.sin(phi)
+    bprime=a*np.sin(phi)+b*np.cos(phi)
+    cprime=c*np.cos(phi)+d*np.sin(phi)
+    dprime=c*np.sin(phi)+d*np.cos(phi)
+    
+    theta=np.arctan2(cprime,aprime)
+    
+    lambda1=np.cos(theta)*aprime+np.sin(theta)*cprime
+    lambda12=np.cos(theta)*bprime+np.sin(theta)*dprime
+    lambda21=-np.sin(theta)*aprime+np.cos(theta)*cprime
+    lambda2=-np.sin(theta)*bprime+np.cos(theta)*dprime
+    
+    lambdaplus=(lambda1+lambda2)/2
+    lambdaminus=(lambda1-lambda2)/2
+    
+    zetaplus=theta-phi
+    zetaminus=-theta-phi
+    
+    locooffseta = a[0]
+    locooffsetc = c[0]
+    
+    locolambdaplus = np.zeros(N-1)
+    locozetaplus = np.zeros(N-1)
+    locolambdaminus = np.zeros(N-1)
+    locozetaminus = np.zeros(N-1)
+    
+    locolambdaplus[0] = lambdaplus[2]
+    locozetaplus[0] = zetaplus[2]
+    
+    locolambdaplus[1] = lambdaplus[1]
+    locozetaplus[1] = zetaplus[1] 
+    
+    
+    locolambdaplus[2:-1] = lambdaplus[3:-1]
+    locozetaplus[2:-1] = zetaplus[3:-1]
+    
+    locolambdaminus[2:-1] = lambdaminus[1:-3]
+    locozetaminus[2:-1] = zetaminus[1:-3]
+    
+    locoL=(locolambdaplus*locolambdaplus+locolambdaminus*locolambdaminus+ \
+           2*locolambdaplus*locolambdaminus* \
+           np.cos(locozetaplus-locozetaminus-2*locozetaplus))**0.5
+    
+    return locoL
 
 def main():
     
-    img=cv2.imread('zoe/wb1127-05-2.jpg')
+    img=cv2.imread('zoe/pi2613-01-1.jpg')
     x1,s1,x2,s2 = CutOut(img)
     thresh = 255*s2.astype(np.uint8)
     kernel = np.ones((3,3),np.uint8)
     edges = cv2.morphologyEx(thresh, cv2.MORPH_GRADIENT, kernel)
 
+    
     nnz=np.nonzero(edges)
     z=len(nnz[0])
-    plt.imshow(thresh)
-    plt.show()    
-    plt.imshow(edges)
-    plt.show()
+#    plt.imshow(thresh)
+#    plt.show()    
+#    plt.imshow(edges)
+#    plt.show()
     #FIND CENTROID
     '''
     important: centroid is done on original image (before edge detection is  
@@ -50,6 +198,7 @@ def main():
     ux=ax-cx
     uy=ay-cy
     u=[ux,uy]
+    
     
     #edge vector
     vx = nnz[1] - cx
@@ -77,13 +226,17 @@ def main():
     plt.scatter(angles, radius,0.1,'b')
     
     ##################################
-    n=20
+    n=10
     ang,rad,fit,fitfn,error = ExtractReg(angles,radius,n)
     print(fit)
     print(np.mean(error))
     plt.plot(ang, rad, 'r')
     plt.scatter(angles,error,0.1,'k')
     ###################################
+#    
+#    locoL = Loco(nnz)
+#    
+#    print(locoL)
     
     
     plt.show()
